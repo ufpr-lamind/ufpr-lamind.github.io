d38a61acd5ed62614ef880617d1c1d3de338e5ea
JulioParodi <juliopepplow@gmail.com>
2018-03-23 12:24:24 -0300

    new posts layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 document.getElementById('logo').addEventListener('click', function() {
-  $.get.window.location.href = "/";
+  window.location.href = "/";
+});
+var showChar = 700;
+var ellipsestext = "...";
+var moretext = "Ler mais >";
+var lesstext = "Mostrar menos";
+
+
+$('.more').each(function() {
+    var content = $(this).html();
+    if(content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content.substr(showChar, content.length - showChar);
+        var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a id="moretext" href="" class="morelink">' + moretext + '</a></span>';
+        $(this).html(html);
+    }
+});
+
+$(".morelink").click(function(){
+  var local = $(this).closest('.post-content').find('h2 > a').attr('href');
+  window.location.href = local;
+  return false;
 });
