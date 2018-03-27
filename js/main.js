d30bdf3a358da1b4168be38422893b5fe4457990
JulioParodi <juliopepplow@gmail.com>
2018-03-27 10:09:41 -0300

    finished posts, added to-top js anchor

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
-document.getElementById('logo').addEventListener('click', function() {
-  window.location.href = "/";
-});
-var showChar = 700;
-var ellipsestext = "...";
-var moretext = "Ler mais >";
-var lesstext = "Mostrar menos";
-
+var showChar = 560;
+var ellipsesText = "...";
+var moreText = "Ler mais >";
 
 $('.more').each(function() {
-    var content = $(this).html();
-    if(content.length > showChar) {
-        var c = content.substr(0, showChar);
-        var h = content.substr(showChar, content.length - showChar);
-        var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a id="moretext" href="" class="morelink">' + moretext + '</a></span>';
-        $(this).html(html);
-    }
+  // $(this).find('p').hide();
+  $(this).find('h1').hide();
+  $(this).find('h2').hide();
+  $(this).find('ul').hide();
+
+  var content = $(this).html();
+  if(content.length > showChar) {
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar, content.length - showChar);
+      var html = c + '<span class="moreellipses">' + ellipsesText+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a id="moreText" href="" class="morelink">' + moreText + '</a></span>';
+      $(this).html(html);
+  }
 });
 
 $(".morelink").click(function(){
   var local = $(this).closest('.post-content').find('h2 > a').attr('href');
   window.location.href = local;
   return false;
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+        $('#to-top').fadeIn()
+    } else {
+        $('#to-top').fadeOut()
+    }
+});
+$("#to-top").click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000)
 });
